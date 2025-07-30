90bcbf2c822ce3faacd3004f87589b1eb48b2786
Your Name <you@example.com>
2025-07-30 10:08:23 -0700

    created the child components and the files for each

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+// the Header.js file
+import React from "react";
+function Header({ name}) {
+    return (
+        <header>
+            <h1>{name}</h1>
+        </header>
+    );
+}
+
+export default Header;
